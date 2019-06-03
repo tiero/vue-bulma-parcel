894552bf773ec5e4913a994f742cfbfaca308d81
Marco Argentieri <marco@vulpem.com>
2019-06-03 23:24:19 +0200

    initial scaffolding

---
+++ src/js/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue';
+import VueResource from 'vue-resource';
+
+
+function beforeMount() {
+}
+
+function mounted() {
+
+}
+
+
+Vue.use(VueResource);
+
+new Vue({
+  el: '#app',
+  data: {},
+  methods: {},
+  beforeMount: beforeMount,
+  mounted: mounted,
+}).$mount('#app')
